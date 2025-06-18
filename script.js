8140da28bc229c35e3f1dc6ab8f934f067d6a4c4
maximiliano1357 <maxitimoner2@gmail.com>
2025-06-18 17:07:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// Ejemplo de código para manejar el envío del formulario (esto es solo un ejemplo básico)
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Formulario enviado. ¡Gracias por contactarnos!');
-});
-
